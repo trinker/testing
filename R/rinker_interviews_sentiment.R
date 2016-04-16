58eaf716ce9d9046f1124d819e1edbf6df97b622
Tyler <tyler.rinker@gmail.com>
2016-04-16 10:56:36 -0400

    test3

---
+++ R/rinker_interviews_sentiment.R
@@ -0,0 +1,10 @@
+#' Interview Sentiment
+#'
+#' A dataset containing sentiment data.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name rinker_interviews_sentiment
+#' @usage data(rinker_interviews_sentiment)
+#' @format A data frame with 427 rows and 4 variables
+NULL
---
+++ R/rinker_observations_sentiment.R
@@ -0,0 +1,10 @@
+#' Observation Sentiment
+#'
+#' A dataset containing sentiment data.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name rinker_observations_sentiment
+#' @usage data(rinker_observations_sentiment)
+#' @format A data frame with 6141 rows and 4 variables
+NULL
