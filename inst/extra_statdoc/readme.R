d717838cbedd5174e95536940f99f06b19030e05
Tyler Rinker <trinker@campuslabs.com>
2016-04-20 11:14:53 -0400

    removed junk

--- inst/extra_statdoc/readme.R
+++
@@ -1,3 +0,0 @@
-<p><img src="https://raw.githubusercontent.com/trinker/testing/master/inst/testing_logo/r_testing.png" width="300"/><br/>
-<p><a href="http://trinker.github.com/testing_dev">testing</a> is a...</p>
-<p>Download the development version of testing <a href="https://github.com/trinker/testing/">here</a>
